3b8645e435ac50fda325ff54663c8a9377c33c21
Baz <bazthirod@gmail.com>
2025-11-12 23:27:17 -0300

    chore(admin): registra modelos para admin

--- pedidos/admin.py
+++ pedidos/admin.py
@@ -1,9 +1,9 @@
-# pedidos/admin.py
 from django.contrib import admin
 from .models import Pedido
 
 @admin.register(Pedido)
 class PedidoAdmin(admin.ModelAdmin):
-    list_display = ("id", "mesa", "cliente", "estado", "creado_en")
+    list_display = ("id", "mesa", "cliente", "estado", "creado_en", "actualizado_en")
+    list_filter  = ("estado", "creado_en")
     search_fields = ("mesa", "cliente", "id")
-    list_filter = ("estado", "creado_en")
+    ordering = ("-creado_en",)
